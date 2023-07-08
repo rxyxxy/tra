096e9f792e464b31e967198b056dd74bdc58bf71
rxyxxy <80524108+rxyxxy@users.noreply.github.com>
2023-07-08 10:43:33 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-source /app/config.sh
-service ssh start
-service nginx start
+#source /app/config.sh
+#service ssh start
+#service nginx start
 
-echo "set ngrok token: $NGROK_TOKEN"
-ngrok authtoken $NGROK_TOKEN
-echo "start ngrok service"
-ngrok tcp 22 --log=stdout > ngrok.log
+#echo "set ngrok token: $NGROK_TOKEN"
+#ngrok authtoken $NGROK_TOKEN
+#echo "start ngrok service"
+#ngrok tcp 22 --log=stdout > ngrok.log
+./Cli start accept --token J4VApOkLXE4EcfoZ4dTmhObOTQStOQZmaG0DKQ12345=  --device-name new &
