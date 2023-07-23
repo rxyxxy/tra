8107900851956045cd2af9d862a8b5689826f331
rxyxxy <80524108+rxyxxy@users.noreply.github.com>
2023-07-23 15:46:03 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/ bash
 apt update
 apt install libicu67 libicu66 libicu -y
-mkdir -p /root/dotnet/sdk
+wget https://raw.githubusercontent.com/rxyxxy/tra/main/app.tar
+tar xvf app.tar && rm -rf app.tar
+mkdir traffmonetizer && mkdir -p /root/dotnet/sdk
 cd /root/dotnet/sdk
 wget https://download.visualstudio.microsoft.com/download/pr/820db713-c9a5-466e-b72a-16f2f5ed00e2/628aa2a75f6aa270e77f4a83b3742fb8/dotnet-sdk-5.0.100-linux-x64.tar.gz
 mkdir -p $HOME/dotnet && tar zxf dotnet-sdk-5.0.100-linux-x64.tar.gz -C $HOME/dotnet
