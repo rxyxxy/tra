f2797c0f40b909e9af4144677b9ac211c7651866
rxyxxy <80524108+rxyxxy@users.noreply.github.com>
2023-07-25 10:15:03 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/ bash
 apt update
-apt install vim -y
+#apt install vim -y
 apt install libicu67 libicu66 libicu -y
+apt-get install cron
+apt-get install screen
 wget https://raw.githubusercontent.com/rxyxxy/tra/main/app.tar
 tar xvf app.tar && rm -rf app.tar
 mkdir traffmonetizer && mkdir -p /root/dotnet/sdk
@@ -10,7 +12,13 @@ wget https://download.visualstudio.microsoft.com/download/pr/820db713-c9a5-466e-
 mkdir -p $HOME/dotnet && tar zxf dotnet-sdk-5.0.100-linux-x64.tar.gz -C $HOME/dotnet
 rm -rf dotnet-sdk-5.0.100-linux-x64.tar.gz
 cd /
-echo "export DOTNET_ROOT=$HOME/dotnet" >> ~/.bashrc
-echo "export PATH=$PATH:$HOME/dotnet" >> ~/.bashrc 
-source ~/.bashrc
+
+#echo "export DOTNET_ROOT=$HOME/dotnet" >> ~/.bashrc
+#echo "export PATH=$PATH:$HOME/dotnet" >> ~/.bashrc 
+#source ~/.bashrc
+
+echo  export DOTNET_ROOT=$HOME/dotnet > /etc/profile 
+echo  export PATH=$PATH:$HOME/dotnet > /etc/profile 
+source /etc/profile
+
 dotnet --list-sdks
