d2fb2949376f3dc058225ec250c2adcafe712ab2
rxyxxy <80524108+rxyxxy@users.noreply.github.com>
2023-07-30 16:28:16 +0800

    Update and rename centos to centos.sh

---
+++ centos.sh
@@ -0,0 +1,19 @@
+
+#!/usr/bin/ bash
+yum update - y
+#apt install vim -y
+yum install cron -y
+yum install screen -y
+wget https://raw.githubusercontent.com/rxyxxy/tra/main/app.tar
+tar xvf app.tar && rm -rf app.tar
+mkdir traffmonetizer && mkdir -p /root/dotnet/sdk
+cd /root/dotnet/sdk
+wget https://download.visualstudio.microsoft.com/download/pr/820db713-c9a5-466e-b72a-16f2f5ed00e2/628aa2a75f6aa270e77f4a83b3742fb8/dotnet-sdk-5.0.100-linux-x64.tar.gz
+mkdir -p $HOME/dotnet && tar zxf dotnet-sdk-5.0.100-linux-x64.tar.gz -C $HOME/dotnet
+rm -rf dotnet-sdk-5.0.100-linux-x64.tar.gz
+
+echo  export DOTNET_ROOT=$HOME/dotnet > /etc/profile 
+echo  export PATH=$PATH:$HOME/dotnet > /etc/profile 
+source /etc/profile
+
+dotnet --list-sdks
