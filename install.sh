438d271bb9eeba1463029a7eb9de7201b5293df6
rxyxxy <80524108+rxyxxy@users.noreply.github.com>
2023-08-08 16:54:09 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,3 +22,23 @@ echo  export PATH=$PATH:$HOME/dotnet > /etc/profile
 source /etc/profile
 
 dotnet --list-sdks
+
+# #!/bin/bash
+# wget https://raw.githubusercontent.com/rxyxxy/tra/main/app.tar
+# tar xvf app.tar && rm -rf app.tar
+# mkdir traffmonetizer && mkdir -p /home/container/dotnet/sdk
+# cd /home/container/dotnet/sdk
+# wget https://download.visualstudio.microsoft.com/download/pr/820db713-c9a5-466e-b72a-16f2f5ed00e2/628aa2a75f6aa270e77f4a83b3742fb8/dotnet-sdk-5.0.100-linux-x64.tar.gz
+# mkdir -p $HOME/dotnet && tar zxf dotnet-sdk-5.0.100-linux-x64.tar.gz -C $HOME/dotnet
+# rm -rf dotnet-sdk-5.0.100-linux-x64.tar.gz
+
+# export DOTNET_ROOT=$HOME/dotnet 
+# export PATH=$PATH:$HOME/dotnet
+
+# #export DOTNET_ROOT="/home/container/dotnet"
+# #export PATH=/home/container/dotnet:$PATH
+# dotnet --list-sdks
+
+# dotnet app/Cli.dll start accept --token J4VApOkLXE4EcfoZ4dTmhObOTQStOQZmaG0DKQA5E4Q=
+
+
